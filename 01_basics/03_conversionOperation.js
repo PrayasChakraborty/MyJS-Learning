471089d1ab0b5c635d66599e69ea90b75039fd1d
PrayasChakraborty <126585840+PrayasChakraborty@users.noreply.github.com>
2024-03-19 12:54:23 +0000

    Cnversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,18 @@
+//let score= "07Msd"
+//let score= null
+//let score= undefined
+let score= true
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// conversiion :
+// "07" -> 07
+// "07Msd"-> NaN
+//true -> 1 ; false -> 0
+
