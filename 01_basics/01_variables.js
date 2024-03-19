49bfe465f20f78cca1e708eeb16cbfec5e9d4882
PrayasChakraborty <126585840+PrayasChakraborty@users.noreply.github.com>
2024-03-19 12:41:16 +0000

    Last updated

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId= 11445443
+let accountEmail = "prayas@gmail.com"
+var accountPassword = "12345"    // var is not used recent days.It block scope and funcional scope.
+accountCity = "Kolkata"
+
+let accountState;
+
+// accountId = 2 : not allowed 
+
+
+
+accountEmail = "chakraborty@gmail.com"
+accountPassword = "2125478"
+accountCity = "Howrah"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,8 @@
+console.log(3+3);
+console.log("Prayas");
+// alart(3+3)  this can be used in web de consol but not in editor environment
+
+
+let name ="Prayas"
+let age = 21
+let isLoggedIn = false
